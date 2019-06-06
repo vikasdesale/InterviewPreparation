4679a6e484c6fe47408b604153236aff0b4e74fd
root <vikas.desaleu@gmail.com>
2019-06-06 22:41:49 +0530

    CProgramming-2.playingwithcharachters

---
+++ CProgramming/HackerRank/2.playingwithcharachters.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    char ch;
+    char s[100];
+    char sen[100];
+    scanf("%c",&ch);
+    scanf("%s",s);
+    scanf("\n");
+    scanf("%[^\n]%*sen",sen);
+    printf("%c\n",ch);
+    printf("%s\n",s);
+    printf("%s",sen);
+
+
+    return 0;
+}
+
+
