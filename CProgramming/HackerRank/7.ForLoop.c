63a1e92f43755f93ec737f28d72787ff7280014f
root <vikas.desaleu@gmail.com>
2019-06-07 23:34:10 +0530

    CProgramming-7.ForLoop

---
+++ CProgramming/HackerRank/7.ForLoop.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+
+int main() 
+{
+    int a, b,i;
+    char *stringArray[] = {"one","two","three","four","five","six","seven","eight","nine"};
+    scanf("%d\n%d", &a, &b);
+  	// Complete the code.
+    for(i=a;i<=b;i++){
+        if(i<=9){
+          printf("%s\n", stringArray[i-1]); 
+        }else{
+          if (i % 2 == 0) {
+            printf("even\n");
+          } else {
+            printf("odd\n");
+          }
+        }
+    }
+    
+    return 0;
+}
+
+
