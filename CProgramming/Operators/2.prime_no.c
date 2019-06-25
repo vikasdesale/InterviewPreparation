e66724f94afa98ba4d0388e35097a6706029f7cb
vikas.desaleu@gmail.com <vikas.desaleu@gmail.com>
2019-06-25 19:40:05 +0530

    CProgramming:1.prime_no_improved

--- CProgramming/Operators/2.prime_no.c
+++ CProgramming/Operators/2.prime_no.c
@@ -10,3 +10,23 @@ char* primality(int n) {
     }
 return flag ? "Prime" : "Not prime";
 }
+
+
+/*impored solution log n(1/2)  */
+
+char* primality(int n) {
+    int flag=1;
+       if(n==2){
+        return "Prime";
+    }
+    if(n==1 || (n&1)==0){
+        return "Not prime";
+    }
+ 
+    for(int i=3;i<=sqrt(n);i+=2){
+         if(n%i==0)
+          {flag=0;
+          break;}
+    }
+return flag ? "Prime" : "Not prime";
+}
